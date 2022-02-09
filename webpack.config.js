b86317c47a470067befbd49af814da61cc51d429
steve_tsao <steve_tsao@neithnet.con>
2022-02-09 17:01:24 +0800

    postcss

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,17 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            },
+          },
+          ,
+          { loader: 'postcss-loader' },
+        ],
       },
       {
         test: /\.(jpg|png|gif)/,
